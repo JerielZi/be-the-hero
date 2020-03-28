a0cc3e9d19f6050b6faa23345cd112a0479060d1
Jeriel <jeriel_zinga@hotmail.com>
2020-03-28 01:48:53 +0000

    Mobile da aplicação

---
+++ mobile/App.js
@@ -0,0 +1,15 @@
+import 'intl';
+
+import 'intl/locale-data/jsonp/pt-PT';
+
+import React from 'react';
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
+
+
